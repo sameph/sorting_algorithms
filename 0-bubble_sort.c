9603edaa78141192bb78bf41cf96feb53cee00ff
sameph <samuelephrem2012@gmail.com>
2023-07-25 06:49:18 -0700

    fixed errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,20 +7,35 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	size_t i, j, len = size;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL || len < 2)
+		return;
+
+	for (i = 0; i < len - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < len - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap_int(array[j], array[j + 1]);
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 	}
 }
+/**
+ * swap_int - swap two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: nothing
+ */
+void swap_int(int a, int b)
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
